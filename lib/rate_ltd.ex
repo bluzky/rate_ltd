255942b796b47e315c88cda73a5effd81cae9527
Dung Nguyen <bluesky.1289@gmail.com>
2025-05-30 17:55:33 +0700

    setup project

---
+++ lib/rate_ltd.ex
@@ -0,0 +1,18 @@
+defmodule RateLtd do
+  @moduledoc """
+  Documentation for `RateLtd`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RateLtd.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rate_ltd_test.exs
@@ -0,0 +1,8 @@
+defmodule RateLtdTest do
+  use ExUnit.Case
+  doctest RateLtd
+
+  test "greets the world" do
+    assert RateLtd.hello() == :world
+  end
+end
